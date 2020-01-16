e7eeb914c420011e1a767a876a8d79c20e565e1a
Serhii Yevtushok <ggazila@gmail.com>
2020-01-16 16:15:45 +0200

    checkstyle

--- src/main/java/mate/academy/internetshop/controller/RegistrationController.java
+++ src/main/java/mate/academy/internetshop/controller/RegistrationController.java
@@ -1,13 +1,12 @@
 package mate.academy.internetshop.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import mate.academy.internetshop.lib.Inject;
 import mate.academy.internetshop.model.Bucket;
