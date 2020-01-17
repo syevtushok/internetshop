69252fbfb476fadb4383bf2fcc7d54870b6ea98f
Serhii Yevtushok <ggazila@gmail.com>
2020-01-17 11:03:24 +0200

    replace way to kill session :)

--- src/main/java/mate/academy/internetshop/controller/LogoutController.java
+++ src/main/java/mate/academy/internetshop/controller/LogoutController.java
@@ -8,15 +8,13 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @WebServlet("/logout")
 public class LogoutController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        HttpSession session = req.getSession();
-        session.setAttribute("userId", "");
+        req.getSession().invalidate();
         for (Cookie cookie : req.getCookies()) {
             if (cookie.getName().equals("MATE")) {
                 cookie.setMaxAge(0);
